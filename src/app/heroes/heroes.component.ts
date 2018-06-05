aff8ccbd24174b7896594957cf4d768edd8ea28e
rmeghlan <rmeghlan@wdtablesystems.com>
2018-06-05 11:01:34 +0530

    Heroes component implemented

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +9,17 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = {id: 1, name: 'Windstorm'};
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 
 
