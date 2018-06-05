083f01538955fffbeacf6c7ba436d0a5f37809f8
rmeghlan <rmeghlan@wdtablesystems.com>
2018-06-05 11:10:50 +0530

    Hero Detail component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
